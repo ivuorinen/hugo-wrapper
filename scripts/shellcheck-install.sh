eeb627e77896c185e52eecb493cf18ea5b0c1d72
Ismo Vuorinen <ismo@ivuorinen.net>
2023-06-19 23:47:56 +0300

    Update shellcheck version

--- scripts/shellcheck-install.sh
+++ scripts/shellcheck-install.sh
@@ -3,7 +3,7 @@
 set -o errexit
 set -o pipefail
 
-SHELLCHECK_VERSION=v0.7.1
+SHELLCHECK_VERSION=v0.9.0
 
 curl -sSLO https://github.com/koalaman/shellcheck/releases/download/"${SHELLCHECK_VERSION}"/shellcheck-"${SHELLCHECK_VERSION}".linux.x86_64.tar.xz
 tar xf shellcheck-"${SHELLCHECK_VERSION}".linux.x86_64.tar.xz
